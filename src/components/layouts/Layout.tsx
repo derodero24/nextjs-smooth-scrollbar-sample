70cb2b81071ae457561ddbd8f028f49b716b56c4
derodero24 <k.itsh20524abc@gmail.com>
2022-11-04 18:39:40 +0900

    add Fixed Element

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -22,10 +22,17 @@ export default function Layout(props: { children: ReactNode }) {
   }, []);
 
   return (
-    <div data-scrollbar className="flex flex-col w-screen h-screen">
-      <Header />
-      <main className="grow">{props.children}</main>
-      <Footer />
-    </div>
+    <>
+      <div data-scrollbar className="flex flex-col w-screen h-screen">
+        <Header />
+        <main className="grow">{props.children}</main>
+        <Footer />
+      </div>
+
+      {/* fixed elements must be placed outside the data-scrollbar */}
+      <div className="fixed bottom-8 right-16 z-30 bg-blue-500 text-white px-4 rounded-md">
+        Fixed Element
+      </div>
+    </>
   );
 }
